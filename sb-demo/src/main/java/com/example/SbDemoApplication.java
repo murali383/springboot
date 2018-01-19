72a577c3920a1901e17722d3a72f61087d0e6484
murali <murali@10.0.0.215>
2018-01-19 12:58:35 -0500

    Code & Dependencies changes.

--- sb-demo/src/main/java/com/example/SbDemoApplication.java
+++ sb-demo/src/main/java/com/example/SbDemoApplication.java
@@ -1,10 +1,36 @@
 package com.example;
 
+import java.net.InetAddress;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SbDemoApplication {
+	
+	@RequestMapping("/")
+	public String msg(){
+		
+		return "App is up & running...";
+	}
+	
+	@RequestMapping("/details")
+	public String details(){
+		
+		String details = "No host details found.";
+		try {
+			InetAddress inetAddress = InetAddress.getLocalHost();
+			details = inetAddress.getHostAddress() + " : " + inetAddress.getHostName();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		return details;
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SbDemoApplication.class, args);
